bb7e2bbab4d5155d7bf860f51c81869bc6bfb71d
Peter Elliott <p.elliott332@gmail.com>
2018-01-27 13:26:34 -0700

    added to instance

--- gameserver/instance.py
+++ gameserver/instance.py
@@ -3,17 +3,21 @@
 class Instance:
     def __init__(self, surface_map):
         self.map = surface_map
-        self.cars = []
+        players = []
         self.alive = True
 
-    def add_car(self, car):
-        self.cars.append(car)
+    def add_player(self, player):
+        self.players.append(player)
 
     def loop():
         while self.alive:
-            for car in cars:
-                surface = self.map[x][y]
-                car.update(surface)
+            for player in players:
+                surface = self.map[player.car.x][player.car.y]
+                player.car.update(surface)
 
+                for player in players:
+                    #TODO call send functions
+                    pass
+            
     def kill(self):
         self.alive = False
