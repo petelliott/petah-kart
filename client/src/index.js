e59411b8ac4e34f01cd27c1c3491a64843e84437
Kyle Hennig <henn.kyle@outlook.com>
2018-01-27 13:24:17 -0700

    Add PixiJS

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,9 @@
 // The entry point into the project.
+window.addEventListener('load', () => {
+  let type = 'WebGL';
+  if (!PIXI.utils.isWebGLSupported()) {
+    type = 'canvas';
+  }
+
+  PIXI.utils.sayHello(type);
+});
