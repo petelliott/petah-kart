10f927d6435c4baf434ce1af70ed0e2c0adf6c92
Jacob Reckhard <jacobreckhard@gmail.com>
2018-01-27 12:53:48 -0700

    made template

--- web/web.py
+++ web/web.py
@@ -2,17 +2,19 @@ import tornado
 import json
 
 
-class MainHandler(tornado.web.RequestHandler):
-
+class JoinHandler(tornado.web.RequestHandler):
     def get(self):
         return True
 
+
+class NewGameHandler(tornado.web.RequestHandler):
     def post(self):
         return True
 
 
 if __name__ == "__main__":
-    app = tornado.web.Application([(r"/", MainHandler), ])
+    app = tornado.web.Application(
+        [(r"/join", JoinHandler), (r"/new", NewGameHandler)])
     try:
         app.listen(8888)
         tornado.ioloop.IOLoop.current().start()
