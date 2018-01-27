6c64a4200e60f59eba162caea753451f52f84e1f
Peter Elliott <p.elliott332@gmail.com>
2018-01-27 16:05:01 -0700

    fixed instance.py for multiple updates

--- gameserver/instance.py
+++ gameserver/instance.py
@@ -4,7 +4,7 @@ import json
 class Instance:
     def __init__(self, surface_map):
         self.map = surface_map
-        players = []
+        self.players = []
         self.alive = True
 
     def add_player(self, player):
@@ -12,19 +12,19 @@ class Instance:
 
     def loop():
         while self.alive:
-            for player in players:
+            for player in self.players:
                 surface = self.map[player.car.x][player.car.y]
                 player.car.update(surface)
-            
-            message = {
+
+            message = [{
                 "velx": player.car.vx,
                 "vely": player.car.vy,
                 "posx": player.car.x,
                 "posy": player.car.y,
-                "id"  : id(player) 
-            }
+                "id":   id(player)
+            } for player in self.players]
 
-            for player in players:
+            for player in self.players:
                 player.write_message(json.dumps(message))
 
     def kill(self):
