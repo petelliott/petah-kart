3554da5fe61f86f3c2091e9627ce83ddb640f81c
Jarrett Yu <jarrett.yu21@gmail.com>
2018-01-27 14:22:02 -0700

    added int values for turn and thrust

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,7 @@
 // The entry point into the project.
+let turn = 0;
+let thrust = 0;
+
 window.addEventListener('load', () => {
   let type = 'WebGL';
   if (!PIXI.utils.isWebGLSupported()) {
@@ -13,20 +16,25 @@ window.addEventListener('keydown', (event) => {
 
   switch (keyID) {
     case 'a':
+      turn = -1;
       console.log('left');
       break;
     case 'd':
+      turn = 1;
       console.log('right');
       break;
     case 'w':
+      thrust = 1;
       console.log('accelerate');
       break;
     case 's':
+      thrust = -1;
       console.log('decelerate');
       break;
     default:
       console.log('invalid input');
   }
+  console.log(`${turn.toString()}, ${thrust.toString()}`);
 }, false);
 
 window.addEventListener('keyup', (event) => {
@@ -34,18 +42,23 @@ window.addEventListener('keyup', (event) => {
 
   switch (keyID) {
     case 'a':
+      turn = 0;
       console.log('stop left');
       break;
     case 'd':
+      turn = 0;
       console.log('stop right');
       break;
     case 'w':
+      thrust = 0;
       console.log('stop accelerate');
       break;
     case 's':
+      thrust = 0;
       console.log('stop decelerate');
       break;
     default:
       console.log('stop invalid input');
   }
+  console.log(`${turn.toString()}, ${thrust.toString()}`);
 }, false);
