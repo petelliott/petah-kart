f2c4163464e7161901c69b4a730bc5b5d3953f2b
cmput274 <cmput274@ualberta.ca>
2018-01-27 12:48:12 -0700

    Set up basic layout

--- web/web.py
+++ web/web.py
@@ -1,2 +1,20 @@
 import tornado
 import json
+
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        return True
+
+    def post(self):
+        return True
+
+
+if __name__ == "__main__":
+    app = tornado.web.Application([(r"/", MainHandler), ])
+    try:
+        app.listen(8888)
+        tornado.ioloop.IOLoop.current().start()
+    except KeyboardInterrupt:
+        print("server exited")
