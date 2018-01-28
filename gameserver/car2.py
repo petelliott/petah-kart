c8de6cb4efb52b6450fcdc16d96656e2b47d6503
Peter Elliott <p.elliott332@gmail.com>
2018-01-28 07:14:18 -0700

    fixed steering added braking

--- gameserver/car2.py
+++ gameserver/car2.py
@@ -4,9 +4,11 @@ import time
 import collide
 
 MAX_SPEED = 10
-ACCEL_CURVE = 5
+ACCEL_CURVE = 10
 MASS = 1
 DUMB_CONST = 0.1
+TURNCONST = 1
+BRAKE_FORCE = 6
 
 
 class Car:
@@ -45,10 +47,22 @@ class Car:
         self.last_time = time.time()
 
         t = self.throttle_curve()
-        b = 0  # self.throttle_curve()  # TODO: add brake curve
+        
+        if math.hypot(*self.vel) != 0:
+            b = self.brakes * BRAKE_FORCE
+        else:
+            b = 0
+        
+        ft = (
+            -self.vel[1] * TURNCONST * (1 if self.wtheta > 0 else -1 if self.wtheta < 0 else 0),
+            self.vel[0] * TURNCONST * (1 if self.wtheta > 0 else -1 if self.wtheta < 0 else 0)
+        )
+
+        if self.wtheta != 0:
+            print(ft)
         fv = (
-            (t - b - surface[2]) * math.cos(self.theta),
-            (t - b - surface[2]) * math.sin(self.theta)
+            (t - b - surface[2]) * math.cos(self.theta) + ft[0],
+            (t - b - surface[2]) * math.sin(self.theta) + ft[1]
         )
 
         self.pos = (
@@ -61,9 +75,12 @@ class Car:
             self.vel[1] + (fv[1] / MASS) * delta
         )
 
-        vel = math.sqrt(self.vel[0]**2 + self.vel[1]**2)
+        # vel = math.sqrt(self.vel[0]**2 + self.vel[1]**2)
 
-        self.theta = self.theta + self.wtheta * vel * DUMB_CONST
+        # self.theta = self.theta + self.wtheta * vel * DUMB_CONST
+
+        if self.vel[1] != 0 or self.vel[0] != 0:
+            self.theta = math.atan2(self.vel[1], self.vel[0])
 
         for car in otherCars:
             if collide.hit_cars(self, car):
