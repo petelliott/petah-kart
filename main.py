7a0e4c46d513e38007e403d9c783b0f79f2301b5
Kyle Hennig <henn.kyle@outlook.com>
2018-01-28 08:08:54 -0700

    Wait for user input to terminate the server

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ try:
         web = subprocess.Popen(["python3", "web"])
         game = subprocess.Popen(["python3", "gameserver", "maps"])
         subprocess.Popen(["http", "POST", "localhost:8888/new", "map=Map0.json", "player_count=3"])
+    input()
 except KeyboardInterrupt:
     pass
 except BaseException as error:
