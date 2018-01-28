a92da5fc88341d95ed55f0a138842624c4eeb13a
Kyle Hennig <henn.kyle@outlook.com>
2018-01-27 22:47:53 -0700

    Allow for cars to be rotated

--- client/src/index.js
+++ client/src/index.js
@@ -25,40 +25,26 @@ function setup() {
   app.stage.addChild(sprite);
 }
 
-cars = [];
+// A map of car ids to sprites.
+let idsToSprites = new Map();
+
 // Cars have an id, x, y, vx, vy, and angle.
-function setCarPositions(newCars) {
-  newCars.forEach((newCar) => {
+function setCarPositions(cars) {
+  cars.forEach((car) => {
     // Create a new car if it doesn't exist.
-    if (!doesCarExist(newCar.id)) {
+    if (!idsToSprites.has(car.id)) {
       let sprite = new PIXI.Sprite(PIXI.loader.resources["images/car_blue_1.png"].texture);
-      sprite.x = newCar.x;
-      sprite.y = newCar.y;
+      sprite.x = car.x;
+      sprite.y = car.y;
+      sprite.rotation = car.angle;
       app.stage.addChild(sprite);
-      cars.push(sprite);
+      idsToSprites.set(id, sprite);
       return;
     }
     // Otherwise, update the location of the old car.
-    let car = getCarByID(newCar.id);
-    car.x = newCar.x;
-    car.y = newCar.y;
+    let carSprite = idsToSprites.get(car.id);
+    carSprite.x = car.x;
+    carSprite.y = car.y;
+    carSprite.rotation = car.angle;
   });
-}
-
-function doesCarExist(carID) {
-  for (let car of cars) {
-    if (car.id === cardID) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function getCarByID(carID) {
-  for (let car of cars) {
-    if (car.id === cardID) {
-      return car;
-    }
-  }
-  throw new Error("The requested car does not exist.");
 }
