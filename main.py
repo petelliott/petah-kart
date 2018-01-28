add17298deaba1e6093870d94b6295414231ee81
Jacob Reckhard <jacobreckhard@gmail.com>
2018-01-27 23:01:37 -0700

    fixed problem in script

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import subprocess
 
 web = subprocess.Popen(["python3", "web"], stdout=subprocess.PIPE)
-game = subprocess.Popen(["python3", "gameserver"], stdout=subprocess.PIPE)
+game = subprocess.Popen(
+    ["python3", "gameserver", "filepath_for_map"], stdout=subprocess.PIPE)
 try:
     input()
 except KeyboardInterrupt:
