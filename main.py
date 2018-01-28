d5ff1e205ef3f82c077c7559c9053294fe8a12e3
Jacob Reckhard <jacobreckhard@gmail.com>
2018-01-28 04:09:11 -0700

    improved run script

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 import subprocess
 
-
-def webErr(out):
-    print("#### WEB ####")
-    print(out)
-    print("#### END ####")
-
-
-def gameErr(out):
-    print("$$$$ GAME $$$$")
-    print(out)
-    print("$$$$ END $$$$")
-
-
 web = subprocess.Popen(["python3", "web"])
 game = subprocess.Popen(
     ["python3", "gameserver", "maps"])
+subprocess.Popen(
+    ["http", "POST", "localhost:8888/new", "map=Map0.json", "player_count=3"])
+
 try:
     input()
 except KeyboardInterrupt:
