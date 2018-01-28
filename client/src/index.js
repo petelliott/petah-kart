565b220280ea03285cfacabbb64878c044ad7fb0
Kyle Hennig <henn.kyle@outlook.com>
2018-01-27 22:35:59 -0700

    Permit cars to be spawned and moved to new locations

--- client/src/index.js
+++ client/src/index.js
@@ -25,12 +25,40 @@ function setup() {
   app.stage.addChild(sprite);
 }
 
+cars = [];
 // Cars have an id, x, y, vx, vy, and angle.
-function setCarPositions(cars) {
-  cars.forEach((car) => {
-    let sprite = new PIXI.Sprite(PIXI.loader.resources["images/car_blue_1.png"].texture);
-    sprite.x = car.x;
-    sprite.y = car.y;
-    app.stage.addChild(cat);
+function setCarPositions(newCars) {
+  newCars.forEach((newCar) => {
+    // Create a new car if it doesn't exist.
+    if (!doesCarExist(newCar.id)) {
+      let sprite = new PIXI.Sprite(PIXI.loader.resources["images/car_blue_1.png"].texture);
+      sprite.x = newCar.x;
+      sprite.y = newCar.y;
+      app.stage.addChild(sprite);
+      cars.push(sprite);
+      return;
+    }
+    // Otherwise, update the location of the old car.
+    let car = getCarByID(newCar.id);
+    car.x = newCar.x;
+    car.y = newCar.y;
   });
+}
+
+function doesCarExist(carID) {
+  for (let car of cars) {
+    if (car.id === cardID) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function getCarByID(carID) {
+  for (let car of cars) {
+    if (car.id === cardID) {
+      return car;
+    }
+  }
+  throw new Error("The requested car does not exist.");
 }
