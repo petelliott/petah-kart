048ad2c97c51843b3d89ba69d00a37dd438d32d7
Jacob Reckhard <jacobreckhard@gmail.com>
2018-01-27 20:25:29 -0700

    refactored file to fix kyles stupid lines

--- client/src/UIUX/ui.js
+++ client/src/UIUX/ui.js
@@ -1,37 +1,13 @@
 const webServerLocation = 'localhost:8888';
 
-// The functions for the UI and page changes
-
-function clickJoin() {
-  console.log('join');
-  const a = prompt('enter server code thing');
-  joinGame(a);
-}
-
-function clickCreate() {
-  console.log('create');
-  newGame(0, 3);
-}
-
-function clickCredits() {
-  console.log('credits');
-}
-
-function clickSettings() {
-  console.log('settings');
-}
-
-// player enters the game ID to join here
-function enterId() {
-
-}
-
 // joingame
 // enter game ID to join that game
 function joinGame(gameId) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', `${webServerLocation}/join/${gameId}`, true); // tyoe, location, isAsync
   xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.responseType = 'document';
+
   xhr.onload = () => {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
@@ -44,12 +20,6 @@ function joinGame(gameId) {
   };
   xhr.send();
 }
-
-// player chooses map, max players and passes into newgame
-function createGame() {
-
-}
-
 // takes the game ID from newgame as a callback and displays it
 function displayId(gameId) {
 
@@ -76,4 +46,34 @@ function newGame(mapId, playerCount) {
     map: mapId, //             the server expects this form.
     player_count: playerCount,
   }));
+}
+
+// The functions for the UI and page changes
+function clickJoin() {
+  console.log('join');
+  const a = prompt('enter server code thing');
+  joinGame(a);
+}
+
+function clickCreate() {
+  console.log('create');
+  newGame(0, 3);
+}
+
+function clickCredits() {
+  console.log('credits');
+}
+
+function clickSettings() {
+  console.log('settings');
+}
+
+// player enters the game ID to join here
+function enterId() {
+
+}
+
+// player chooses map, max players and passes into newgame
+function createGame() {
+
 }
